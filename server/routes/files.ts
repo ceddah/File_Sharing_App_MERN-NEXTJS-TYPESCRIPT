a74295e4f2be9ecd718878da49778193a1e3c1cc
ceddahm <cedanik1@gmail.com>
2021-12-01 11:52:13 +0100

    user can download file

--- server/routes/files.ts
+++ server/routes/files.ts
@@ -2,6 +2,7 @@ import express from "express";
 import multer from "multer";
 import { UploadApiResponse, v2 as cloudinary } from "cloudinary";
 import File from "../models/File";
+import https from "https";
 
 const router = express.Router();
 const storage = multer.diskStorage({});
@@ -45,6 +46,41 @@ router.post("/upload", upload.single("myFile"), async (req, res) => {
     const { message } = error as Error;
     console.log(message);
     res.status(500).json({ message: "Server Error has occurred..." });
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const file = await File.findById(id);
+    if (!file) {
+      return res.status(404).json({ message: "File does not exist." });
+    }
+
+    return res.status(200).json({
+      name: file.filename,
+      format: file.format,
+      sizeInBytes: file.sizeInBytes,
+      id: file.id,
+    });
+  } catch (error) {
+    return res.status(500).json({ message: "Server error has occurred." });
+  }
+});
+
+router.get("/:id/download", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const file = await File.findById(id);
+    if (!file) {
+      return res.status(404).json({ message: "File does not exist." });
+    }
+
+    https.get(file.secure_url, (fileStream) => {
+      fileStream.pipe(res);
+    });
+  } catch (error) {
+    return res.status(500).json({ message: "Server error has occurred." });
   }
 });
 
